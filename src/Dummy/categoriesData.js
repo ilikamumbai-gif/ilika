7f8212735753dde35df00203507affc45d94b3eb
kanvipanchal <kanvipanchal2004@gmail.com>
2026-02-18 10:17:31 +0530

    images

--- src/Dummy/categoriesData.js
+++ src/Dummy/categoriesData.js
@@ -1,15 +1,15 @@
 export const categoriesData = [
-  { name: "Ilika New", icon: "/src/assets/Images/New-Cat.jpg" ,link:"/"},
-  { name: "Gift", icon: "/src/assets/Images/Gifting-Cate.jpg" ,link:"/"},
-  { name: "Face", icon: "/src/assets/Images/Face-Cat.jpg" ,link:"/skin"},
-  { name: "Hair", icon: "/src/assets/Images/Hair-Cat.jpg" ,link:"/hair"},
-  { name: "Body", icon: "/src/assets/Images/Body-Cat.jpg" ,link:"/skin"},
-  { name: "Appliances", icon: "/src/assets/Images/Appliance-Cat.jpg" ,link:"/grooming"},
-  { name: "Offers", icon: "/src/assets/Images/Offer-Cat.jpg" ,link:"/offer"},
+  { name: "Ilika New", icon: "/Images/New-Cat.jpg" ,link:"/"},
+  { name: "Gift", icon: "/Images/Gifting-Cate.jpg" ,link:"/"},
+  { name: "Face", icon: "/Images/Face-Cat.jpg" ,link:"/skin"},
+  { name: "Hair", icon: "/Images/Hair-Cat.jpg" ,link:"/hair"},
+  { name: "Body", icon: "/Images/Body-Cat.jpg" ,link:"/skin"},
+  { name: "Appliances", icon: "/Images/Appliance-Cat.jpg" ,link:"/grooming"},
+  { name: "Offers", icon: "/Images/Offer-Cat.jpg" ,link:"/offer"},
 ];
 
 export const IngredientsData =[
-     { name: "Ilika New", icon: "/src/assets/Images/body-Cat.jpg" },
-  { name: "Gift", icon: "/src/assets/Images/Gifting-Cate.jpg" },
-  { name: "Face", icon: "/src/assets/Images/Face-Cat.jpg" },
+     { name: "Ilika New", icon: "/Images/Body-Cat.jpg" },
+  { name: "Gift", icon: "/Images/Gifting-Cate.jpg" },
+  { name: "Face", icon: "/Images/Face-Cat.jpg" },
 ]
